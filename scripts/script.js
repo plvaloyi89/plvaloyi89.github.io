5299110533b944054a31b558cf0243dcaceffeeb
plvaloyi89 <plvaloyi@gmail.com>
2023-01-02 10:57:06 -0500

    details page edit

--- scripts/script.js
+++ scripts/script.js
@@ -87,7 +87,8 @@ function getProjectID(){
     document.getElementById("project_title").innerHTML = details.get('name')
     document.getElementById("project_industry").innerHTML = details.get('industry')
     document.getElementById("project_link_title").innerHTML = "Link"
-    document.getElementById("project_link").innerHTML = details.get('link')
+    document.getElementById("project_link").innerHTML = details.get('name')
+    document.getElementById("project_link").href = details.get('link')
     document.getElementById("project_intro").innerHTML = details.get('description')
     document.getElementById("project_overview").innerHTML = "Overview"
     document.getElementById("project_details_overview").innerHTML = details.get('overview')
